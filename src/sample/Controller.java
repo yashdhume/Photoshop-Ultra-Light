bce0d0dfe2652b7af1f6c29515ccbefdbef86512
yashdhume <yash_dhume@yahoo.ca>
2019-01-29 23:54:07 -0500

    First Commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class Controller {
+    int counter = 0;
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Button btn1;
+
+    @FXML
+    private Label lab1;
+
+    @FXML
+    void display(ActionEvent event) {
+        counter++;
+        lab1.setText("Button Clicked "+ counter);
+    }
+
+
+}

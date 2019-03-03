326266423bfe986fb65fff3fb201a9eb67b6ef6e
yashdhume <yash_dhume@yahoo.ca>
2019-03-02 19:38:04 -0500

    Added Maven Project and Picture Search

---
+++ src/main/java/ImageScraper/ImageScraper.java
@@ -0,0 +1,46 @@
+package ImageScraper;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ImageScraper {
+    public static ArrayList<String> getImageArray(String search, int numOfSearchResults) throws IOException{
+        ArrayList<String> resultUrls = new ArrayList<String>();
+        String userAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+        String url = "https://www.google.com/search?site=imghp&tbm=isch&source=hp&q="+search+"&gws_rd=cr";
+
+        try {
+            Document doc = Jsoup.connect(url).userAgent(userAgent).referrer("https://www.google.com/").get();
+
+            Elements elements = doc.select("div.rg_meta");
+            int count =0;
+            JSONObject jsonObject;
+            for (Element element : elements) {
+                if (count==numOfSearchResults){
+                    break;
+                }
+                if (element.childNodeSize() > 0) {
+                    jsonObject = (JSONObject) new JSONParser().parse(element.childNode(0).toString());
+                    resultUrls.add((String) jsonObject.get("ou"));
+                }
+                count++;
+            }
+
+            System.out.println("number of results: " + resultUrls.size());
+
+            //for (String imageUrl : resultUrls) System.out.println(imageUrl);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return resultUrls;
+    }
+}
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,22 +1,86 @@
 package sample;
 
+import ImageScraper.ImageScraper;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main extends Application {
-
+    ArrayList<Image> images = new ArrayList<Image>();
+    ArrayList<ImageView> imageView = new ArrayList<ImageView>();
+    int numOfSearchResults = 10;
+    public void loadImage(String textField)throws IOException{
+        ArrayList<String> googleImagesLinks = ImageScraper.getImageArray(textField, numOfSearchResults);
+        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        for (int i = 0; i < googleImagesLinks.size(); i++) {
+            images.add(new Image(googleImagesLinks.get(i)));
+        }
+        for (int i = 0; i < images.size(); i++) {
+            imageView.add(new ImageView(images.get(i)));
+            imageView.get(i).setFitHeight(1000);
+            imageView.get(i).setFitWidth(500);
+            imageView.get(i).setPreserveRatio(true);
+        }
+    }
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1280, 800));
+        final TextField textField = new TextField();
+        Button btn = new Button("Search");
+
+
+
+        final FlowPane flowPane = new FlowPane();
+        flowPane.setPadding(new Insets(5, 5, 5, 5));
+        flowPane.setVgap(5);
+        flowPane.setHgap(5);
+        flowPane.setPrefWrapLength(5);
+        flowPane.setAlignment(Pos.CENTER);
+        flowPane.getChildren().add(textField);
+        flowPane.getChildren().add(btn);
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                try {
+                    loadImage(textField.getText());
+                    for (int i = 0; i < imageView.size(); i++) {
+                        flowPane.getChildren().add(imageView.get(i));
+                    }
+                }
+                catch (IOException ex){
+
+                }
+            }
+        });
+        final ScrollPane scroll = new ScrollPane();
+        scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);    // Horizontal scroll bar
+        scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);    // Vertical scroll bar
+        scroll.setContent(flowPane);
+
+        Scene scene = new Scene(scroll, 500, 500);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }

4c38832247acc25fe17ca4f68e5d2ba2060ff0f8
jwdjj <jsscawdjj@gmail.com>
2019-03-18 23:23:56 -0400

    Fixed Brush & Move button issue and Implemented customize page sizing on new layers

--- src/main/java/Main/Controller.java
+++ src/main/java/Main/Controller.java
@@ -8,7 +8,6 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;

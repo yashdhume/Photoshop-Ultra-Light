89ccabc32ebc8f05fb613d6d7aaa437be7416549
Kashif <kashif.h77@gmail.com>
2019-03-18 22:46:45 -0400

    Add ability to move Solid's

--- src/main/java/Layers/SolidLayer.java
+++ src/main/java/Layers/SolidLayer.java
@@ -3,6 +3,7 @@ package Layers;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import org.opencv.core.Point;
 
 
 public class SolidLayer extends Layer {
@@ -15,6 +16,14 @@ public class SolidLayer extends Layer {
     public SolidLayer(String name, double width, double height, Color color){
         super(name);
         rectangle = new Rectangle((int)width, (int)height, color);
+    }
+
+    @Override
+    public void setLocation(Point p) {
+        location.x = p.x - rectangle.getWidth()/2;
+        location.y = p.y - rectangle.getHeight()/2;
+        rectangle.setX(location.x);
+        rectangle.setY(location.y);
     }
 
     @Override

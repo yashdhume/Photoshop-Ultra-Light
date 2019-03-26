c668eb38483767bce7a80ab873fde1e586c37516
Kashif <kashif.h77@gmail.com>
2019-03-25 22:27:48 -0400

    Fixed Admin Controls

--- src/main/java/Global/SwitchButton.java
+++ src/main/java/Global/SwitchButton.java
@@ -11,8 +11,11 @@ import java.util.ArrayList;
 
 public class SwitchButton extends Label {
     private SimpleBooleanProperty switchedOn = new SimpleBooleanProperty(true);
-
+    private int index;
+    ArrayList<Account> accounts;
     public SwitchButton(int count, ArrayList<Account> account, Server server) {
+        this.accounts = account;
+        this.index = count;
         Button switchBtn = new Button();
         switchBtn.setPrefWidth(40);
         switchBtn.setOnAction(t -> switchedOn.set(!switchedOn.get()));
@@ -23,14 +26,32 @@ public class SwitchButton extends Label {
                 setStyle("-fx-background-color: green;-fx-text-fill:white;-fx-border-radius: 20 20 20 20;");
                 setContentDisplay(ContentDisplay.RIGHT);
                 account.get(count).setVip(true);
+                server.saveFile(accounts);
+                //switchedOn.setValue(true);
             } else {
                 setText("OFF");
                 setStyle("-fx-background-color: red;-fx-text-fill:white;-fx-border-radius: 20 20 20 20;");
                 setContentDisplay(ContentDisplay.LEFT);
                 account.get(count).setVip(false);
+                server.saveFile(accounts);
+                //switchedOn.setValue(false);
             }
-            server.saveFile(account);
         });
-        switchedOn.set(false);
+        switchedOn.set(account.get(count).isVip());
+        setStyle(switchedOn.getValue().booleanValue());
+        server.saveFile(accounts);
+    }
+    private void setStyle(boolean value){
+        if (value) {
+            setText("ON");
+            setStyle("-fx-background-color: green;-fx-text-fill:white;-fx-border-radius: 20 20 20 20;");
+            setContentDisplay(ContentDisplay.RIGHT);
+            //switchedOn.setValue(true);
+        } else {
+            setText("OFF");
+            setStyle("-fx-background-color: red;-fx-text-fill:white;-fx-border-radius: 20 20 20 20;");
+            setContentDisplay(ContentDisplay.LEFT);
+            //switchedOn.setValue(false);
+        }
     }
 }
